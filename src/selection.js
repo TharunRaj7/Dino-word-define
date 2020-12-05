95e31215e05939bf6b5e8ff7e6f980a759ff3ed5
Himanshu Jain <hkj4@dj4.edu>
2020-12-05 16:44:16 -0500

    made the modal

--- src/selection.js
+++ src/selection.js
@@ -15,6 +15,79 @@ chrome.runtime.onMessage.addListener(
       definitionJSON = JSON.parse(request.definition);
       console.log(definitionJSON);
       definitionView.innerText = definitionJSON[0]["meanings"][0]["definitions"][0]["definition"]
-      document.body.insertBefore(definitionView, document.body.firstChild);
+      var word= definitionJSON[0]["word"]
+  var name = definitionView.innerText;
+     var audioLink= definitionJSON[0]["phonetics"][0]["audio"]
+  var exampleModal = getExampleModal();
+
+  // Init the modal if it hasn't been already.
+  if (!exampleModal) { exampleModal = initExampleModal(); }
+  var html =
+      '<div class="modal-header">' +
+        '<h5 class="modal-title" id="exampleModalLabel">'+word+'</h5>' +
+      '</div>' + 
+      '<div class="modal-body">' +
+        name +
+      '</div>' + 
+      '<div class="modal-body">' +
+      '<audio controls> <source src='+audioLink+ ' type="audio/mpeg">Your browser does not support the audio element. </audio>' +
+      '</div>';
+      var linebreak= document.createElement('br')
+      var footer= document.createElement('div');
+      footer.classList.add('modal-footer');
+      var button = document.createElement('button');
+      button.classList.add('btn');
+      button.classList.add('btn-primary');
+      button.innerHTML="Add it to your list";
+      var button2 = document.createElement('button');
+      button2.classList.add('btn');
+      button2.classList.add('btn-secondary');
+      button2.innerHTML="Close";
+      button.addEventListener("click", check);
+      button2.addEventListener("click", remove);
+      footer.appendChild(button);
+      footer.appendChild(button2);
+      
+
+  setExampleModalContent(html, footer, linebreak);
+
+
+  // Show the modal.
+  // jQuery(exampleModal).modal('show');
+      document.body.insertBefore(exampleModal, document.body.firstChild);
     }
   });
+
+function check(){
+  console.log("hello")
+}
+
+  function getExampleModal() {
+    return document.getElementById('exampleModal');
+  }
+
+  function remove(){
+    // getExampleModal().querySelector('.modal-content').removeChild(footer);
+    getExampleModal().querySelector('.modal-content').innerHTML = "";
+  }
+  
+  function setExampleModalContent(html, footer, linebreak) {
+    getExampleModal().querySelector('.modal-content').innerHTML = html;
+    getExampleModal().querySelector('.modal-content').appendChild(footer);
+  }
+  
+  function initExampleModal() {
+    var modal = document.createElement('div');
+    modal.classList.add('modal', 'fade');
+    modal.setAttribute('id', 'exampleModal');
+    modal.setAttribute('tabindex', '-1');
+    modal.setAttribute('role', 'dialog');
+    modal.setAttribute('aria-labelledby', 'exampleModalLabel');
+    modal.setAttribute('aria-hidden', 'true');
+    modal.innerHTML =
+          '<div class="modal-dialog" role="document">' +
+            '<div class="modal-content"></div>' +
+          '</div>';
+    document.body.appendChild(modal);
+    return modal;
+  }

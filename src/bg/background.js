a5de8c16fa85ae502276a82b47cd49db2d761960
Tharun Raj <tm272@duke.edu>
2020-12-06 12:54:00 +0800

    fixed css bugs and popup

--- src/bg/background.js
+++ src/bg/background.js
@@ -24,7 +24,7 @@ getSelectionDefinition = function (word) {
     var definition = Http.responseText;
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
       chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello", "definition": definition}, function(response) {
-        console.log(response.farewell)
+        //console.log(response.wordDefined);
       });  
     });
   }
